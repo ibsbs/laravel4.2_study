fecd6524f6db0e371f6eb0e54a80d8f2843ee9c6
yboker <8236138@qq.com>
2014-09-02 17:55:57 +0800

    add

---
+++ .env.php
@@ -0,0 +1,13 @@
+<?php
+return array
+(
+    'APP_DEBUG'          => false,
+    'DATABASE_DRIVER'    => 'mysql',
+    'DATABASE_HOST'      => 'localhost',
+    'DATABASE_DATABASE'  => 'my_iduoqian',
+    'DATABASE_USERNAME'  => 'root',
+    'DATABASE_PASSWORD'  => '',
+    'DATABASE_CHARSET'   => 'utf8',
+    'DATABASE_COLLATION' => 'utf8_unicode_ci',
+    'DATABASE_PREFIX'    => 'bzphp_'
+);
