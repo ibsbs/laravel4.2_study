671b4e6daca795fc1a250eef67fbcb01ebee0248
yboker <8236138@qq.com>
2014-09-15 21:29:19 +0800

    add

--- app/views/layouts/layout.blade.php
+++ app/views/layouts/layout.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        @yield('title')
-        @yield('meta')
-        @yield('css')
-        @yield('js_head')
-    </head>
-        @yield('content')
-        @yield('js_body')
-    <body>
-    </body>
+  <head>
+    @yield('title')
+    @yield('meta')
+    @yield('css')
+    @yield('js_head')
+  </head>
+  <body>
+    @yield('content')
+    @yield('js_body')
+  <body>
+</html>
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,30 +1,34 @@
 @extends('layouts.layout')
-
 @section('title') 
-   <title>这是一个标题</title>
+  <title>这是一个标题</title>
 @stop
-
 @section('meta')
-    <meta name="keywords" content="杨波的bootstrap后台" />
-    <meta name="description" content="杨波的bootstrap3.0后台" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="keywords" content="杨波的bootstrap后台" />
+  <meta name="description" content="杨波的bootstrap3.0后台" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 @stop
-
 @section('css')
   <!-- basic styles -->
-        <link href="{{asset('admin')}}/assets/css/bootstrap.min.css" rel="stylesheet" />
-        <link rel="stylesheet" href="{{asset('admin')}}/assets/css/font-awesome.min.css" />
-        <!--[if IE 7]>
-          <link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css" />
-        <![endif]-->
-        <!-- page specific plugin styles -->
-        <!-- fonts -->
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" />
-        <!-- ace styles -->
-        <link rel="stylesheet" href="{{asset('admin')}}/assets/css/ace.min.css" />
-        <link rel="stylesheet" href="{{asset('admin')}}/assets/css/ace-rtl.min.css" />
-        <!--[if lte IE 8]>
-          <link rel="stylesheet" href="{{asset('admin')}}/assets/css/ace-ie.min.css" />
-        <![endif]-->
-        <!-- inline styles related to this page -->
+  <link href="{{asset('admin')}}/assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="{{asset('admin')}}/assets/css/font-awesome.min.css" />
+  <!--[if IE 7]>
+    <link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css" />
+  <![endif]-->
+  <!-- page specific plugin styles -->
+  <!-- fonts -->
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" />
+  <!-- ace styles -->
+  <link rel="stylesheet" href="{{asset('admin')}}/assets/css/ace.min.css" />
+  <link rel="stylesheet" href="{{asset('admin')}}/assets/css/ace-rtl.min.css" />
+  <!--[if lte IE 8]>
+    <link rel="stylesheet" href="{{asset('admin')}}/assets/css/ace-ie.min.css" />
+  <![endif]-->
+  <!-- inline styles related to this page -->
+@stop
+@section('js_head')
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="assets/js/html5shiv.js"></script>
+  <script src="assets/js/respond.min.js"></script>
+  <![endif]-->
 @stop
