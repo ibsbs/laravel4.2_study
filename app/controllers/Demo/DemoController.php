5a25b27587cb8a5411a13380907c8831a16b6e9a
yboker <8236138@qq.com>
2014-09-25 23:54:56 +0800

    add

--- app/controllers/Demo/DemoController.php
+++ app/controllers/Demo/DemoController.php
@@ -1,32 +1,14 @@
 <?php
 namespace Demo;
 
+use Kint;
+
 class DemoController extends \BaseController
 {
     public function  getAboutArray()
     {
-        //$array = [0=>'aa', 1=>'bb', 3=>'cc'];
-        //var_dump($array);
-        //$value = decbin(86);
-        // $val = '991F0C6E504546A25744E3DF0D26A441';
-        $value = 85;
-        for($i=86; $i<=255; $i++) {
-            //$now_i = decbin($i);
-            $now_i = $i;
-            //$now_next = decbin($i+1);
-            //$value  = $now_i^$now_next;
-
-            $value = $now_i^$value;
-        }
-        //$value_1 = md5($value);
-        $value_1 = md5($value, true);
-        echo $value_1;
-        $hex = hex2bin($value_1);
-
-        //$value_2 = md5($hex);
-        //$value = (string)$value;
+        $array = [0=>'aa', 1=>'bb', 3=>'cc'];
+        Kint::dump($array);
         
-        echo  $hex, '----', $value_1, '----', $value;
-        //var_dump(decbin(85));
     }
 }
